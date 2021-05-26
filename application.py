210800be056070a714001cbdc730e48523a1f457
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-05-26 14:18:33 -0600

    added postgresql

--- application.py
+++ application.py
@@ -10,7 +10,8 @@ import jwt
 
 load_dotenv()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    'DATABASE_URL').replace('postgres', 'postgresql')
 
 CORS(app)
 
--- migrations/env.py
+++ migrations/env.py
@@ -13,7 +13,8 @@ load_dotenv()
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option('sqlalchemy.url', os.environ.get('DATABASE_URL'))
+config.set_main_option('sqlalchemy.url', os.environ.get(
+    'DATABASE_URL').replace('postgres', 'postgresql'))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
