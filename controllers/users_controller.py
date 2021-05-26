c314cb9d6095ab8620f60a4c826554416f7fd738
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-05-26 08:48:45 -0600

    removed encoding.

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -24,10 +24,7 @@ def create_user():
 
     models.db.session.add(user)
     models.db.session.commit()
-
-    encrypted_id = jwt.encode({"user_id": user.id}, os.environ.get(
-        'JWT_SECRET'), algorithm="HS256").decode('utf-8')
-    return {"user": user.to_json(), "user_id": encrypted_id}
+    return {"user": user.to_json(), "user_id": user.id}
 
 
 def login():
@@ -36,21 +33,17 @@ def login():
         return {"message": "User not found"}, 404
 
     if bcrypt.check_password_hash(user.password, request.json["password"]):
-        encrypted_id = jwt.encode({"user_id": user.id}, os.environ.get(
-            'JWT_SECRET'), algorithm="HS256")
-        print(encrypted_id)
-        return {"user": user.to_json(), "user_id": encrypted_id}
+        return {"user": user.to_json(), "user_id": user.id}
     else:
         return {"message": "Password incorrect"}, 401
 
 
 def verify_user():
-    print(request.headers["Authorization"])
-    print(os.environ.get(
-        'JWT_SECRET'))
-    decrypted_id = jwt.decode(request.headers["Authorization"], os.environ.get(
-        'JWT_SECRET'), algorithms=["HS256"])["user_id"]
-    user = models.User.query.filter_by(id=decrypted_id).first()
-    if not user:
-        return {"message": "user not found"}, 404
-    return {"user": user.to_json()}
+    if request.headers["Authorization"]:
+        user = models.User.query.filter_by(
+            id=request.headers["Authorization"]).first()
+        if not user:
+            return {"message": "user not found"}, 404
+        return {"user": user.to_json()}
+    else:
+        return {"message": "User id is required."}, 400
